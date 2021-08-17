9d89ec33b313ffdb9d29943a0afc4167d7b17571
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-08-17 11:38:36 +0200

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,71 @@
-const Home = () => {
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+import path from "path";
+import styles from "../src/styles/Home.module.css";
+import Layout from "../src/components/Layout";
+import { AtlasLink } from "../src/components/Btns/AtlasLink";
+import { GetStaticProps } from "next";
+import { getMDInfo } from "../src/helpers/functions";
+
+interface HomeProps {
+  atlasInfo: {
+    article: string;
+    frontMatter: {
+      date: string;
+      mainTitle: string;
+      shortTitle: string;
+      pdfUrl: string;
+    };
+  }[];
+}
+
+const Home: React.FC<HomeProps> = ({ atlasInfo }) => {
+  const Links = atlasInfo.map((atlas) => (
+    <AtlasLink
+      key={atlas.article}
+      linkTo={atlas.article}
+      style={{ height: "200px" }}
+    >
+      <div>{atlas.frontMatter.shortTitle} </div>
+    </AtlasLink>
+  ));
 
   return (
-    <h1>Helseatlas</h1>
+    <Layout>
+      <div className={styles.full_bleed}>
+        <div className={styles.banner_article}>
+          <div className={styles.banner_article__content}>
+            <h2>Likeverdige helsetjenester - uansett hvor du bor?</h2>
+            <p>
+              Helseatlas sammenlikner befolkningens bruk av helsetjenester
+              gjennom interaktive kart, rapporter og faktaark.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.full_bleed} ${styles.buttons_container}`}>
+        <div className={`${styles.buttons}`}>
+          <h2>Helseatlas</h2>
+          <div className={styles.block_buttons} >
+            {Links}
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const atlasDir = path.join(process.cwd(), "_posts/atlas");
+  const atlasInfo = getMDInfo(atlasDir);
+
+  return {
+    props: {
+      atlasInfo,
+    },
+  };
 };
 
 export default Home;
---
+++ src/components/Btns/AtlasLink.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { CSSProperties, ReactNode } from "react";
+import styles from "../../styles/Home.module.css";
+
+interface Props {
+  children: ReactNode;
+  linkTo: string;
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const AtlasLink = (props: Props) => {
+  const { children, style, className, linkTo } = props;
+
+  return (
+    <div
+      className={`${styles.block_button} ${className ?? ""}`}
+      style={{ ...style }}
+    >
+      <Link href={`/${linkTo}`} >
+        <a style={{ width: "100%", height: "100%" }}>{children}</a>
+      </Link>
+    </div>
+  );
+};
