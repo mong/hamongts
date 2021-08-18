229612c31c11baf9f0c5e4675509086c26fe20c6
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-08-18 15:59:04 +0200

    debug

--- pages/[atlas].tsx
+++ pages/[atlas].tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps, GetStaticPaths } from "next";
 import path from "path";
 import { useRouter } from "next/router";
-import { getMDInfo } from "../src/helpers/functions";
+//import { getMDInfo } from "../src/helpers/functions";
 
 const Post = (props) => {
   props.content;
@@ -10,6 +10,40 @@ const Post = (props) => {
   return <p>HelseAtlas: {atlas}</p>;
 };
 export const getStaticProps: GetStaticProps = async (context) => {
+
+  const getMDInfo = (dirPath: string) => {
+    const files = fs.readdirSync(dirPath, "utf-8");
+    const info = files
+      .filter((fn) => fn.endsWith(".md"))
+      .map((fn) => {
+        const filePath = path.join(dirPath, fn);
+        const rawContent = fs.readFileSync(filePath, {
+          encoding: "utf-8",
+        });
+
+        const { data } = matter(rawContent);
+        const article = fn.replace(/\.md$/, "");
+        return {
+          article,
+          frontMatter: data,
+        };
+      })
+      .sort(
+        (a, b) =>
+          Date.parse(b.frontMatter.date) - Date.parse(a.frontMatter.date)
+      )
+      .map((article) => {
+        return {
+          ...article,
+          frontMatter: {
+            ...article.frontMatter,
+          },
+        };
+      });
+
+    return info;
+  };
+
   const atlasDir = path.join(process.cwd(), "_posts/atlas");
   const atlasInfo = getMDInfo(atlasDir);
 
@@ -21,6 +55,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
+
   const atlasDir = path.join(process.cwd(), "_posts/atlas");
   const atlasInfo = getMDInfo(atlasDir);
   const paths = atlasInfo.map((Info) => ({ params: { atlas: Info.article } }));
