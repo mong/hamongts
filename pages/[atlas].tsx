b5decebf14ca4fca951a2e9bd9f415ebd65a547c
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-08-18 16:02:06 +0200

    debug

--- pages/[atlas].tsx
+++ pages/[atlas].tsx
@@ -1,6 +1,9 @@
 import { GetStaticProps, GetStaticPaths } from "next";
 import path from "path";
 import { useRouter } from "next/router";
+import fs from "fs"
+import matter from "gray-matter";
+
 //import { getMDInfo } from "../src/helpers/functions";
 
 const Post = (props) => {
@@ -10,6 +13,8 @@ const Post = (props) => {
   return <p>HelseAtlas: {atlas}</p>;
 };
 export const getStaticProps: GetStaticProps = async (context) => {
+
+
 
   const getMDInfo = (dirPath: string) => {
     const files = fs.readdirSync(dirPath, "utf-8");
@@ -44,6 +49,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     return info;
   };
 
+
   const atlasDir = path.join(process.cwd(), "_posts/atlas");
   const atlasInfo = getMDInfo(atlasDir);
 
@@ -55,6 +61,40 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
+
+
+  const getMDInfo = (dirPath: string) => {
+    const files = fs.readdirSync(dirPath, "utf-8");
+    const info = files
+      .filter((fn) => fn.endsWith(".md"))
+      .map((fn) => {
+        const filePath = path.join(dirPath, fn);
+        const rawContent = fs.readFileSync(filePath, {
+          encoding: "utf-8",
+        });
+
+        const { data } = matter(rawContent);
+        const article = fn.replace(/\.md$/, "");
+        return {
+          article,
+          frontMatter: data,
+        };
+      })
+      .sort(
+        (a, b) =>
+          Date.parse(b.frontMatter.date) - Date.parse(a.frontMatter.date)
+      )
+      .map((article) => {
+        return {
+          ...article,
+          frontMatter: {
+            ...article.frontMatter,
+          },
+        };
+      });
+
+    return info;
+  };
 
   const atlasDir = path.join(process.cwd(), "_posts/atlas");
   const atlasInfo = getMDInfo(atlasDir);
