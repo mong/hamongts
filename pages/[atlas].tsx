49eb4df398746a5a13c6893c9b052f2616848819
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-08-20 13:24:38 +0200

    test layout

--- pages/[atlas].tsx
+++ pages/[atlas].tsx
@@ -1,29 +1,67 @@
-import { GetStaticProps, GetStaticPaths } from "next";
 import path from "path";
+import { GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/router";
-import { getMDInfo } from "../src/helpers/functions/markdownHelpers";
+import fs from "fs";
+import matter from "gray-matter";
+import marked from "marked";
 
-const Post = (props) => {
-  props.content;
+import { Atlas } from "../src/components/Atlas";
+import { remark } from "remark";
+import html from "remark-html";
+import Layout from "../src/components/Layout";
+import { TopBanner } from "../src/components/Atlas/topBanner";
+
+export async function markdownToHtml(markdown: string) {
+  const result = await remark().use(html).process(markdown);
+  return result.toString();
+}
+
+interface AtlasPageProps {
+  content: string;
+  frontMatter: {
+    date: string;
+    mainTitle: string;
+    shortTitle: string;
+    pdfUrl: string;
+  };
+}
+
+const AtlasPage: React.FC<AtlasPageProps> = ({ content, frontMatter }) => {
   const router = useRouter();
-  const { atlas } = router.query;
-  return <p>HelseAtlas: {atlas}</p>;
+  return (
+    <>
+      <Layout>
+        <div>
+          <TopBanner {...frontMatter} />
+        </div>
+        <div
+          style={{ width: "90%", textAlign: "justify", margin: "auto" }}
+          className="atlas-content"
+        >
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }} />
+        </div>
+      </Layout>
+    </>
+  );
 };
 export const getStaticProps: GetStaticProps = async (context) => {
   const atlasDir = path.join(process.cwd(), "_posts/atlas");
-  const atlasInfo = getMDInfo(atlasDir);
 
+  const fullPath = path.join(atlasDir, `${context.params.atlas}.md`);
+  const file = fs.readFileSync(fullPath);
+  const { content, data } = matter(file);
+
+  const htmlcontent = await markdownToHtml(content || "");
   return {
-    props: {
-      content: "content",
-    },
+    props: { content: htmlcontent, frontMatter: { ...data } },
   };
 };
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
   const atlasDir = path.join(process.cwd(), "_posts/atlas");
-  const atlasInfo = getMDInfo(atlasDir);
-  const paths = atlasInfo.map((Info) => ({ params: { atlas: Info.article } }));
+  const paths = fs
+    .readdirSync(atlasDir)
+    .map((Info) => ({ params: { atlas: Info.replace(/.md?$/, "") } }));
 
   return {
     paths,
@@ -31,4 +69,4 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
   };
 };
 
-export default Post;
+export default AtlasPage;
