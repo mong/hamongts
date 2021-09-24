ff29c4bd4c7f0ef10e76dfc88733df076670c211
arnfinn <arnfinn.steindal@gmail.com>
2021-09-24 14:48:16 +0200

    Internationalization: no/en

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
     loader: "custom",
   },
   trailingSlash: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["no", "en"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "no",
+    localeDetection: false,
+  },
 };
