9282a7d1ac9062e7eb513203e2ab80080d48e72b
arnfinn <arnfinn.steindal@gmail.com>
2021-10-04 09:20:42 +0200

    barn eng take 2

--- public/en/barn/ia/map.js
+++ public/en/barn/ia/map.js
@@ -1,12 +1,12 @@
 {
 	"template":"Single Map (HTML Edition)",
-	"version":"6.7.1_b1,635 (2013-09-04 1449)",
+	"version":"6.7.4_b1,805 (2016-03-31 1512)",
 	"boundingBox":"-136793.8005 6373968.9399999995 1174717.5105 8014549.86",
 	"layers":[
 	{
 		"type":"base-layer",
 		"id":"_HF_omr_9_(Nordland_OUS_Akershus).shp1",
-		"name":"Opptaksområder",
+		"name":"HF_omr_9_(Nordland_OUS_Akershus).shp",
 		"geometry":"polygon",
 		"url":"_HF_omr_9_(Nordland_OUS_Akershus).shp1.js",
 		"visible":true,
