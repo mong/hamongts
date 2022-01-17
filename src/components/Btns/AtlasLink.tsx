2b9bf222644b9ad5a2f8c5cec0d3c4d6d68e8579
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-01-17 15:55:22 +0100

    Remove double code (#89)

--- src/components/Btns/AtlasLink.tsx
+++ src/components/Btns/AtlasLink.tsx
@@ -23,18 +23,3 @@ export const AtlasLink = (props: Props) => {
     </div>
   );
 };
-
-export const AtlasLinkEn = (props: Props) => {
-  const { children, style, className, linkTo } = props;
-
-  return (
-    <div
-      className={`${styles.block_button} ${className ?? ""}`}
-      style={{ ...style }}
-    >
-      <Link href={`/en/${linkTo}`}>
-        <a style={{ width: "100%", height: "100%" }}>{children}</a>
-      </Link>
-    </div>
-  );
-};
