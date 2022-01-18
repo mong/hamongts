1407470084bb24d4ae089293462b2f8372b96384
arnfinn <arnfinn.steindal@gmail.com>
2022-01-18 16:08:43 +0100

    add one more test

--- test/cypress/integration/home.spec.tsx
+++ test/cypress/integration/home.spec.tsx
@@ -8,6 +8,11 @@ context("Home Page", () => {
   it("should render the home page and display a message", () => {
     cy.get("h2").contains("Likeverdige helsetjenester - uansett hvor du bor?");
   });
+
+  it("should visit an atlas", () => {
+    cy.visit("/utdatert/kvalitet");
+    cy.get("h1").contains("Helseatlas for kvalitet");
+  });
 });
 
 export {};
