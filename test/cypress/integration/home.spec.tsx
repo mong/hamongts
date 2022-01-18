d919a496737290fba0abee8b3e081045ca0a390f
arnfinn <arnfinn.steindal@gmail.com>
2022-01-18 13:53:31 +0100

    cypress step 1

---
+++ test/cypress/integration/home.spec.tsx
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+context("Home Page", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000");
+    });
+  
+    it("should render the home page and display a message", () => {
+      cy.get("h1").contains("Welcome");
+    });
+  });
+  
+  export {}
