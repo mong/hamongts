e8cff9e7b4145cf1172a59255371716715aba618
yte0 <Yohannes.Tesfay@helse-nord.no>
2022-01-24 12:48:05 +0100

    custom rehype plugin

---
+++ src/helpers/functions/rehypeplugins.tsx
@@ -0,0 +1,21 @@
+import { visit } from "unist-util-visit";
+import type { Root } from "hast";
+
+export const rehypeWrapWithDiv = () => {
+  return (tree) => {
+    console.log(tree)
+    const divWrapper: Root = {
+      type: "root",
+      children: [
+        {
+          type: "element",
+          tagName: "div",
+          children: tree.children,
+          properties: { class: "main-content" }
+        },
+      ],
+    };
+
+    return divWrapper;
+  };
+};
