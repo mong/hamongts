e88650d1fc3451eb98c0abd61d8d22acc1e74d7b
arnfinn <arnfinn.steindal@gmail.com>
2022-01-25 09:46:55 +0100

    expand test

--- test/cypress/integration/home.spec.tsx
+++ test/cypress/integration/home.spec.tsx
@@ -5,7 +5,7 @@ context("Home Page", () => {
     cy.visit("/");
   });
 
-  it("should render the home page and display a message", () => {
+  it("should render the home page", () => {
     cy.get("h2").contains("Likeverdige helsetjenester - uansett hvor du bor?");
     cy.contains("a").click();
   });
@@ -13,6 +13,12 @@ context("Home Page", () => {
   it("should visit an atlas", () => {
     cy.visit("/utdatert/kvalitet");
     cy.get("h1").contains("Helseatlas for kvalitet");
+    cy.contains("Kart").click();
+    cy.get("button").contains("Last ned data");
+  });
+  it("should visit an IA", () => {
+    cy.visit("/utdatert/gyn/ia/index.html");
+    cy.get("button").contains("Last ned data");
   });
 });
 
