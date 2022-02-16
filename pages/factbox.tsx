29ba8e4a005274bb16b945c2024e4c17ffbe3983
YT <54272616+yte0@users.noreply.github.com>
2022-02-16 17:23:53 +0100

    fact box (#157)

---
+++ pages/factbox.tsx
@@ -0,0 +1,22 @@
+import { FactBox } from "../src/components/Factbox";
+
+const FactBoxPage = () => {
+  return (
+    <FactBox
+      id="1"
+      boxTitle="Lorem ipsum dolor sit amet"
+      boxContent="Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud 
+      exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in 
+      reprehenderit in voluptate velit esse cillum dolore 
+      eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia
+      deserunt mollit anim id est laborum"
+    />
+  );
+};
+
+export default FactBoxPage;
