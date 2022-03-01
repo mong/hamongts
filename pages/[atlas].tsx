2cf8b0fd5310d3398a60e9da4394d290d8a70c3e
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-03-01 10:32:13 +0100

    CMS for nye atlas (#146)
    Versjon 0.1

--- pages/[atlas].tsx
+++ pages/[atlas].tsx
@@ -1,82 +1,33 @@
 import path from "path";
 import { GetStaticProps, GetStaticPaths } from "next";
 import fs from "fs";
-
-import ReactMarkdown from "react-markdown";
 import matter from "gray-matter";
-import rehypeToc from "rehype-toc";
-import rehypeSlug from "rehype-slug";
-import rehypeRaw from "rehype-raw";
-import { rehypeWrapWithDiv } from "../src/helpers/functions/rehypeplugins";
-import remarkGfm from "remark-gfm";
-
 import Layout from "../src/components/Layout";
 import { TopBanner } from "../src/components/Atlas/topBanner";
-import { TableOfContents } from "../src/components/toc";
-import { OrderedList } from "../src/components/toc/orderedlist";
-import { ListItem } from "../src/components/toc/listitem";
 import styles from "../src/styles/Atlas.module.css";
 
 interface AtlasPageProps {
-  text: string;
   content: string;
-  frontMatter: {
-    num: string;
-    mainTitle: string;
-    shortTitle: string;
-    pdfUrl: string;
-    ia: boolean;
-    lang: string;
-    report_text: string;
-    map_text: string;
-    ingress: string;
-  };
 }
 
-const AtlasPage: React.FC<AtlasPageProps> = ({ content, frontMatter }) => {
-  const text = `<h1>${frontMatter.mainTitle}</h1><div className=ingress>${frontMatter.ingress}</div>${content}`;
+const AtlasPage: React.FC<AtlasPageProps> = ({ content }) => {
+  const obj = JSON.parse(content);
+
   return (
     <>
-      <Layout lang="no">
+      <Layout lang={obj.lang}>
         <main>
-          <TopBanner {...frontMatter} />
-          <div className={`${styles.atlasContent}`} style={{ display: "flex" }}>
-            <ReactMarkdown
-              rehypePlugins={[
-                rehypeWrapWithDiv,
-                rehypeRaw,
-                rehypeSlug,
-                [
-                  rehypeToc,
-                  {
-                    headings: ["h2", "h3"],
-                  },
-                ],
-              ]}
-              remarkPlugins={[remarkGfm]}
-              components={{
-                nav({ children, className }) {
-                  if (className === "toc") {
-                    return <TableOfContents> {children}</TableOfContents>;
-                  }
-                  return <nav>{children}</nav>;
-                },
-                ol({ children, className }) {
-                  if ((className ?? "").includes("toc")) {
-                    return <OrderedList> {children}</OrderedList>;
-                  }
-                  return <ol>{children}</ol>;
-                },
-                li({ children, className }) {
-                  if ((className ?? "").includes("toc")) {
-                    return <ListItem> {children}</ListItem>;
-                  }
-                  return <li>{children}</li>;
-                },
-              }}
-            >
-              {text}
-            </ReactMarkdown>
+          <TopBanner
+            mainTitle={obj.shortTitle}
+            pdfUrl=""
+            lang={obj.lang}
+            ia={false}
+          />
+          <div className={`${styles.atlasContent}`}>
+            <h1>{obj.mainTitle}</h1>
+            <br />
+            <div className="ingress">{obj.ingress}</div>
+            <>{JSON.stringify(obj.innhold)}</>
           </div>
         </main>
       </Layout>
@@ -85,21 +36,24 @@ const AtlasPage: React.FC<AtlasPageProps> = ({ content, frontMatter }) => {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const atlasDir = path.join(process.cwd(), "_posts/atlas");
-  const fullPath = path.join(atlasDir, `${context.params.atlas}.md`);
+  const fullPath = path.join(
+    process.cwd(),
+    "_posts/atlas",
+    `${context.params.atlas}.json`
+  );
   const file = fs.readFileSync(fullPath);
-  const { content, data } = matter(file);
+  const { content } = matter(file);
 
   return {
-    props: { content, frontMatter: { ...data } },
+    props: { content },
   };
 };
 
-export const getStaticPaths: GetStaticPaths = async (context) => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const atlasDir = path.join(process.cwd(), "_posts/atlas");
   const paths = fs
     .readdirSync(atlasDir)
-    .map((Info) => ({ params: { atlas: Info.replace(/.md?$/, "") } }));
+    .map((Info) => ({ params: { atlas: Info.replace(/.json?$/, "") } }));
 
   return {
     paths,
