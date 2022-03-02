a1183229a8155bc82e6c9c5356247b7dbf8cdd35
arnfinn <arnfinn.steindal@gmail.com>
2022-03-02 12:57:33 +0100

    testing

--- test/cypress/integration/home.spec.tsx
+++ test/cypress/integration/home.spec.tsx
@@ -1,5 +1,7 @@
 /// <reference types="cypress"/>
 
+import { multiply } from "cypress/types/lodash";
+
 context("Home Page", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -60,5 +62,30 @@ context("Modern atlases", () => {
     cy.get("h1").contains("Helseatlas for fødselshjelp 2.0");
   });
 });
+
+context("result box", () => {
+  it("should visit a result box", () => {
+    cy.visit("/resultbox");
+    cy.get("h3").contains("Lorem ipsum dolor sit amet");
+    cy.get("svg").click({ multiple: true, force: true })
+  });
+});
+
+context("factbox", () => {
+  it("should visit a factbox", () => {
+    cy.visit("/factbox");
+    cy.get("div").contains("Lorem ipsum dolor sit amet");
+    cy.get("svg").click({ multiple: true, force: true })
+  });
+});
+
+context("carousel", () => {
+  it("should visit a carousel", () => {
+    cy.visit("/carousel");
+    cy.get("h2").contains("Kroniske inflammatoriske leddsykdommer");
+    cy.get("svg").click({ multiple: true, force: true })
+  });
+});
+
 
 export {};
