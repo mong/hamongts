de7b0f1cd2b864c767c6ffa1950b4a208f90c764
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-03-15 10:02:07 +0100

    Remove popup component. Not used. Reintroduce later if needed (#252)

--- src/helpers/hooks/index.ts
+++ src/helpers/hooks/index.ts
@@ -2,5 +2,3 @@ export * from "./useintersectionobserver";
 export * from "./useisomorphiclayouteffect";
 export * from "./useresizeobserver";
 export * from "./useprevious";
-export * from "./useKeys";
-export * from "./useOnCLickOutside";
