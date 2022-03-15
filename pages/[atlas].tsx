mong/hamongts

commit 73fb7f5bac06670000167ee4585034a0468a63a0

Bruk hovedoverskrift i ID for resultatboks (#258)
* Bruk hovedoverskrift i ID for resultatboks

* reset file

* polish

pages/[atlas].tsx
@@ -56,34 +56,23 @@ const AtlasPage: React.FC<AtlasPageProps> = ({ content, atlasData }) => {
                   const level2Header = (
                     <OrderedList>
                       {cont.level2.map((level2) => {
+                        const level2ID = (cont.level1 + "_" + level2)
+                          .toLowerCase()
+                          .replace(/\s/g, "-");
                         return (
-                          <ListItem
-                            key={level2.toLowerCase().replace(/\s/g, "-")}
-                          >
-                            <a
-                              href={`#${level2
-                                .toLowerCase()
-                                .replace(/\s/g, "-")}`}
-                            >
-                              {level2}
-                            </a>
+                          <ListItem key={level2ID}>
+                            <a href={`#${level2ID}`}></a>
                           </ListItem>
                         );
                       })}
                     </OrderedList>
                   );
-
+                  const level1ID = cont.level1
+                    .toLowerCase()
+                    .replace(/\s/g, "-");
                   return (
-                    <ListItem
-                      key={cont.level1.toLowerCase().replace(/\s/g, "-")}
-                    >
-                      <a
-                        href={`#${cont.level1
-                          .toLowerCase()
-                          .replace(/\s/g, "-")}`}
-                      >
-                        {cont.level1}
-                      </a>
+                    <ListItem key={level1ID}>
+                      <a href={`#${level1ID}`}>{cont.level1}</a>
                       {level2Header}
                     </ListItem>
                   );

src/components/Chapters/index.tsx
@@ -58,9 +58,10 @@ export const Chapters = ({ innhold }: ChaptersProps) => {
 };
 
 const Chapter = ({ innhold, overskrift }: ChapterProps) => {
+  const mainID = overskrift.toLowerCase().replace(/\s/g, "-");
   return (
     <>
-      <h2 id={overskrift.toLowerCase().replace(/\s/g, "-")}>{overskrift}</h2>
+      <h2 id={mainID}>{overskrift}</h2>
       <div>
         {innhold.map((box, index) => {
           const props =
@@ -68,7 +69,10 @@ const Chapter = ({ innhold, overskrift }: ChapterProps) => {
               ? {
                   boxContent: box.tekst,
                   boxTitle: box.overskrift,
-                  id: box.overskrift,
+                  id:
+                    mainID +
+                    "-fact-" +
+                    box.overskrift.toLowerCase().replace(/\s/g, "-"),
                 }
               : box.type === "resultatboks"
               ? {
@@ -76,7 +80,10 @@ const Chapter = ({ innhold, overskrift }: ChapterProps) => {
                   title: box.overskrift,
                   intro: box.ingress,
                   selection: box.utvalg,
-                  id: box.overskrift,
+                  id:
+                    mainID +
+                    "_" +
+                    box.overskrift.toLowerCase().replace(/\s/g, "-"),
                   carousel: box.karusell,
                   xlabel: box.karusell.xlabel,
                   ylabel: box.karusell.ylabel,

src/components/ResultBox/resultbox.tsx
@@ -64,10 +64,7 @@ export const ResultBox: React.FC<ResultBoxProps> = ({
           id={`${id}-header`}
         >
           <div className={styles.resultBoxTitleWrapper}>
-            <h3 id={title.toLocaleLowerCase().replace(/\s/g, "-")}>
-              {" "}
-              {title}{" "}
-            </h3>
+            <h3 id={id}> {title} </h3>
             <Markdown lang={lang}>{intro}</Markdown>
             {figdata && (
               <Abacus

commit a2eb872c17f54977f7fd5a8b41cfed2139fcce40

Fixed bug introduced in #258
Forgot to include level2 text in ToC

pages/[atlas].tsx
@@ -61,7 +61,7 @@ const AtlasPage: React.FC<AtlasPageProps> = ({ content, atlasData }) => {
                           .replace(/\s/g, "-");
                         return (
                           <ListItem key={level2ID}>
-                            <a href={`#${level2ID}`}></a>
+                            <a href={`#${level2ID}`}>{level2}</a>
                           </ListItem>
                         );
                       })}