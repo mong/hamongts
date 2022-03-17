f697ebc87b42f0698ed26b45eb6b0de2944fef76
arnfinn <arnfinn.steindal@gmail.com>
2022-03-17 09:25:32 +0100

    Revert "Remove popup component. Not used. Reintroduce later if needed (#252)"
    This reverts commit de7b0f1cd2b864c767c6ffa1950b4a208f90c764.

--- src/helpers/hooks/index.ts
+++ src/helpers/hooks/index.ts
@@ -2,3 +2,5 @@ export * from "./useintersectionobserver";
 export * from "./useisomorphiclayouteffect";
 export * from "./useresizeobserver";
 export * from "./useprevious";
+export * from "./useKeys";
+export * from "./useOnCLickOutside";
