c68fd2daae8896635eaf0f47d4f492f49e09de1c
arnfinn <arnfinn.steindal@gmail.com>
2022-03-31 15:03:39 +0200

    endret kolonnenavn til kontakter/kont_pr_pas
    For å kunne vise tall i tabell

--- scripts/fix_data.sh
+++ scripts/fix_data.sh
@@ -13,4 +13,12 @@ sed -i 's/\ //g' $i
 
 sed -i 's/ratesnitt/rateSnitt/g' $i
 
+sed -i 's/kontakter_poli/kontakter/g' $i
+sed -i 's/ant_akutt/kontakter/g' $i
+
+sed -i 's/polikont_pr_pas/kont_pr_pas/g' $i
+sed -i 's/primkont_pr_pas/kont_pr_pas/g' $i
+sed -i 's/akutt_pr_pas/kont_pr_pas/g' $i
+
+
 done
