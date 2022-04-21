40d44c9ec2a05b6856933222383bc8a41b4ef4d2
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-04-21 15:46:36 +0200

    use map.x as abacus value to plot (#377)
    * use map.x as abacus value to plot
    
    * fix type
    
    Co-authored-by: yte0 <Yohannes.Tesfay@helse-nord.no>

--- src/components/ResultBox/resultbox.tsx
+++ src/components/ResultBox/resultbox.tsx
@@ -125,6 +125,9 @@ export const ResultBox: React.FC<ResultBoxProps> = ({
   const figdata: AtlasData[] = boxData
     ? boxData.filter((d) => d.type === "data")[0]["data"]
     : null;
+  const abacusX: Exclude<keyof AtlasData, "year" | "bohf"> = boxData
+    .filter((boxd) => boxd.type === "map")
+    .map((boxd) => boxd.x)[0];
 
   const handleChange = (cb: React.Dispatch<React.SetStateAction<boolean>>) =>
     cb((state) => !state);
@@ -156,7 +159,7 @@ export const ResultBox: React.FC<ResultBoxProps> = ({
             {figdata && (
               <Abacus
                 data={figdata}
-                x="rateSnitt"
+                x={abacusX}
                 colorBy="bohf"
                 width={800}
                 height={80}
