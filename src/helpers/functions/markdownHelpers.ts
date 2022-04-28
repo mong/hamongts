mong/hamongts

commit cfbe070dda23daedd4a0b56d4ded4c409c06bf32

new atlas on front page (#445)
* WIP

* add v2 atlas to frontpage

src/helpers/functions/markdownHelpers.ts
@@ -27,7 +27,7 @@ export const getMDInfo = (dirPath: string) => {
     .sort((a, b) => b.frontMatter.num - a.frontMatter.num)
     .map((article) => {
       return {
-        ...article,
+        article: `v1/${article}`,
         frontMatter: {
           ...article.frontMatter,
         },

pages/index.tsx
@@ -1,9 +1,10 @@
 import path from "path";
+import { GetStaticProps } from "next";
+import fs from "fs";
+
 import Layout from "../src/components/Layout";
 import { MainBanner } from "../src/components/MainBanner/MainBanner";
-
 import { AtlasLink } from "../src/components/Btns/AtlasLink";
-import { GetStaticProps } from "next";
 import { getMDInfo } from "../src/helpers/functions/markdownHelpers";
 
 interface HomeProps {
@@ -16,39 +17,20 @@ interface HomeProps {
       date: Date;
     };
   }[];
-  atlasInfoNew: {
-    article: string;
-    frontMatter: {
-      shortTitle: string;
-      image: string;
-      frontpagetext: string;
-      date: Date;
-    };
-  }[];
+  
 }
 
-const Home: React.FC<HomeProps> = ({ atlasInfo, atlasInfoNew }) => {
+const Home: React.FC<HomeProps> = ({ atlasInfo }) => {
   const Links = atlasInfo.map((atlas, i) => (
     <AtlasLink
       key={atlas.article}
-      linkTo={`v1/${atlas.article}`}
+      linkTo={`${atlas.article}`}
       imageSource={atlas.frontMatter.image}
       linkTitle={atlas.frontMatter.shortTitle}
       linkText={atlas.frontMatter.frontpagetext}
       wide={i === 0}
       date={atlas.frontMatter.date}
       newlyUpdated={i === 0}
-      lang={"no"}
-    />
-  ));
-  const LinksNew = atlasInfoNew.map((atlas) => (
-    <AtlasLink
-      key={atlas.article}
-      linkTo={atlas.article}
-      imageSource={atlas.frontMatter.image}
-      linkTitle={atlas.frontMatter.shortTitle}
-      linkText={atlas.frontMatter.frontpagetext}
-      date={atlas.frontMatter.date}
       lang={"no"}
     />
   ));
@@ -68,7 +50,6 @@ const Home: React.FC<HomeProps> = ({ atlasInfo, atlasInfoNew }) => {
             paddingRight: "16px",
           }}
         >
-          {LinksNew}
           {Links}
         </div>
       </main>
@@ -80,12 +61,45 @@ export const getStaticProps: GetStaticProps = async () => {
   const atlasDir = path.join(process.cwd(), "_posts/tidligere_atlas");
   const atlasInfo = getMDInfo(atlasDir);
   const atlasDirNew = path.join(process.cwd(), "_posts/atlas");
-  const atlasInfoNew = getMDInfo(atlasDirNew);
+  const atlasInfoNew = fs
+    .readdirSync(atlasDirNew)
+    .filter((fn) => fn.endsWith(".json"))
+    .map((fn) => {
+      const filePath = path.join(atlasDirNew, fn);
+      const rawContent = fs.readFileSync(filePath, {
+        encoding: "utf-8",
+      });
+      const parsedContent = JSON.parse(rawContent);
+      const {
+        image,
+        frontpagetext,
+        filename,
+        publisert,
+        lang,
+        date,
+        mainTitle,
+        shortTitle,
+      } = parsedContent;
+      if (!publisert) {
+        return null;
+      }
+
+      return {
+        article: `${filename}`,
+        frontMatter: {
+          shortTitle,
+          image,
+          frontpagetext,
+          date,
+          lang: lang === "en" ? lang : "no",
+        },
+      };
+    })
+    .filter((d) => d !== null);
 
   return {
     props: {
-      atlasInfo,
-      atlasInfoNew,
+      atlasInfo: [...atlasInfoNew, ...atlasInfo],
     },
   };
 };

commit a07ffa173f9b291cb78e5901d718a7a05c5471ce

Fix link on first page
Broken with #445

src/helpers/functions/markdownHelpers.ts
@@ -18,7 +18,7 @@ export const getMDInfo = (dirPath: string) => {
           yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }),
         },
       });
-      const article = fn.replace(/\.md$/, "");
+      const article = "v1/" + fn.replace(/\.md$/, "");
       return {
         article,
         frontMatter: data,
@@ -27,7 +27,7 @@ export const getMDInfo = (dirPath: string) => {
     .sort((a, b) => b.frontMatter.num - a.frontMatter.num)
     .map((article) => {
       return {
-        article: `v1/${article}`,
+        ...article,
         frontMatter: {
           ...article.frontMatter,
         },