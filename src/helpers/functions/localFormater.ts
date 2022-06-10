cf11b8970181536e8870823a247d0916fe411298
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-06-10 14:29:45 +0200

    Format on numbers taken from qmongjs (#496)

--- src/helpers/functions/localFormater.ts
+++ src/helpers/functions/localFormater.ts
@@ -1,9 +1,11 @@
-import { formatLocale } from "d3-format";
+import { formatLocale, FormatLocaleDefinition } from "d3-format";
 
-const formatDefinition = {
+const formatDefinition: FormatLocaleDefinition = {
   decimal: ",",
   thousands: "\u202f",
   grouping: [3],
+  currency: ["NOK", ""],
+  percent: "\u202f%",
 };
 
 export const customFormat = formatLocale(formatDefinition).format;
