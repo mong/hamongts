3a37ea9e2d3940304eb0944ca09113b2c254d9a6
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-06-14 17:05:53 +0200

    Removed unused helper hook useResizeObserver (#531)

--- src/helpers/hooks/useprevious.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const usePrevious = <State>(state: State): State => {
-  const ref = React.useRef<State>();
-  React.useEffect(() => {
-    ref.current = state;
-  }, [state]);
-  return ref.current;
-};
